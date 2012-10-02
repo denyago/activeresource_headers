055fddea1a6e17d2b9b6a481211cdaad4c7deb70
Denis Yagofarov <denyago@gmail.com>
2012-10-02 17:36:20 +0300

    Implemented everything needed

--- lib/activeresource_headers/custom_headers.rb
+++ lib/activeresource_headers/custom_headers.rb
@@ -1,5 +1,29 @@
+require 'active_support/concern'
+require 'active_support/core_ext/hash/deep_merge'
+
 module ActiveresourceHeaders
   module CustomHeaders
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+
+      def custom_headers(&block)
+        @custom_headers = block
+      end
+
+      def with_headers(headers={})
+        @with_headers = headers
+        self
+      end
+
+      def connection
+        cn = super
+        headers = @custom_headers ? @custom_headers.call : {}
+        headers = headers.deep_merge((@with_headers || {}))
+        cn.send(:default_header).update(headers)
+        cn
+      end
+    end
 
   end
 end
--- spec/activeresource_headers/custom_headers_spec.rb
+++ spec/activeresource_headers/custom_headers_spec.rb
@@ -2,37 +2,51 @@ require 'spec_helper'
 
 describe ActiveresourceHeaders::CustomHeaders do
 
-  before(:each) do
-    unset_const(:Profile)
-    class Profile < ActiveResource::Base
-      include ActiveresourceHeaders::CustomHeaders
+  require File.expand_path('spec/spec_helper')
 
-      self.site = REMOTE_HOST
+  describe "while using #custom_headers class method" do
+    before(:each) do
+      unset_const(:Profile)
+      class Profile < ActiveResource::Base
+        include ActiveresourceHeaders::CustomHeaders
 
-      custom_headers do
-        {time: Time.now}
+        self.site = REMOTE_HOST
+
+        custom_headers do
+          {time: self.time}
+        end
       end
+      FakeWeb.register_uri(:get, "#{REMOTE_HOST}/profiles.json", body: [].to_json)
+      Profile.stub(:time).and_return("yesterday")
+    end
+
+    it "runs it's block each time" do
+      Profile.find(:all)
+      FakeWeb.last_request[:time].should eq('yesterday')
+
+      Profile.stub(:time).and_return("tomorrow")
+
+      Profile.find(:all)
+      FakeWeb.last_request[:time].should eq('tomorrow')
+    end
+
+    it "deeply merges it's headers from #with_headers method" do
+      Profile.with_headers(time: 'Soon!').find(:all)
+      FakeWeb.last_request[:time].should eq('Soon!')
     end
   end
 
-  it "add class method for defining custom headers in the model" do
-    FakeWeb.register_uri(:get, "#{REMOTE_HOST}/profiles.json", body: [].to_json)
-    Time.stub(:now).and_return("yesterday")
-
-    Profile.find(:all)
-    FakeWeb.last_request[:time].should eq('yesterday')
-
-    Time.stub(:now).and_return("tomorrow")
-
-    Profile.find(:all)
-    FakeWeb.last_request[:time].should eq('tomorrow')
-  end
-
-  it "add method for headers in find request" do
+  it "adds #with_headers chainable method" do
+    unset_const(:Profile)
+    class Profile < ActiveResource::Base
+      include ActiveresourceHeaders::CustomHeaders
+      self.site = REMOTE_HOST
+    end
     FakeWeb.register_uri(:get, "#{REMOTE_HOST}/profiles.json", body: [].to_json)
 
-    Profile.with_headers(time: 'Soon!').find(:all)
-    FakeWeb.last_request[:time].should eq('Soon!')
+    Profile.with_headers('Authorization' => 'OAuth2 token', 'Client-Id' => 'xyz').find(:all)
+    FakeWeb.last_request["authorization"].should eq('OAuth2 token')
+    FakeWeb.last_request["client-id"].should eq('xyz')
   end
 
 end
